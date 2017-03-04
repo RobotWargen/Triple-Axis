aa49a12bf76cbfc70efce4898d06f4c75da572e3
Razvan Chirca <theboywithglasses1303@gmail.com>
2017-03-04 16:24:07 +0200

    Some features added
    Am adaugat sub-meniu, rearanjat fontul.
    Am mai adaugat cateva linii de JS pentru funtionarea acelui sub-meniu.

--- proj - Copy/js/script.js
+++ proj - Copy/js/script.js
@@ -1,19 +1,52 @@
+/*jslint plusplus: true */
+
+//JSLint is a JavaScript program that looks for problems in JavaScript programs. It is a code quality tool.
+
+//Prima functie. Meniu Stanga
+
 function openNav() {
+  "use strict";
   document.getElementById("main-nav").style.width = "250px";
   
 }
 
 function closeNav() {
+  "use strict";
   document.getElementById("main-nav").style.width = "0";
   
 }
 
+// A doua funtie. Meniu Dreapta
+
 function openAccNav() {
+  "use strict";
   document.getElementById("account-nav").style.width = "250px";
   
 }
 
 function closeAccNav() {
+  "use strict";
   document.getElementById("account-nav").style.width = "0";
   
+}
+
+//Meniu Accourdion
+
+// Selectarea Linkului cu clasa "sublinks"
+var accordion = document.getElementsByClassName("sublinks");
+var i; //pentru a nu avea eroarea care spune ca variabila a fost folosita inainte de a fi declarata
+
+for (i = 0; i < accordion.length; i++) {
+  accordion[i].onclick = function () {
+    "use strict";
+    this.classList.toggle("active");
+    
+    var inside = this.nextElementSibling;
+    
+    if (inside.style.maxHeight) {
+      inside.style.maxHeight = null;
+    } else {
+      inside.style.maxHeight = inside.scrollHeight + "px";
+    }
+  };
 }
