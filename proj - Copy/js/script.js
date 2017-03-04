65690135e56cb531b2f4eb61d00ee9296fd8ce50
Razvan Chirca <theboywithglasses1303@gmail.com>
2017-03-04 11:26:30 +0200

    Just added the files

---
+++ proj - Copy/js/script.js
@@ -0,0 +1,19 @@
+function openNav() {
+  document.getElementById("main-nav").style.width = "250px";
+  
+}
+
+function closeNav() {
+  document.getElementById("main-nav").style.width = "0";
+  
+}
+
+function openAccNav() {
+  document.getElementById("account-nav").style.width = "250px";
+  
+}
+
+function closeAccNav() {
+  document.getElementById("account-nav").style.width = "0";
+  
+}
